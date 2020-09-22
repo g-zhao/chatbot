9ed9b6f2a5741ad392b4c9ddabac5dfef18e4476
g-zhao <zhao.gordon@gmail.com>
2020-09-21 23:51:46 -0500

    added contactInfo controller

--- src/main/java/com/gzhao/chatbot/controller/ContactInfoController.java
+++ src/main/java/com/gzhao/chatbot/controller/ContactInfoController.java
@@ -1,8 +1,21 @@
 package com.gzhao.chatbot.controller;
 
+import com.gzhao.chatbot.data.ContactInfoDAO;
+import com.gzhao.chatbot.model.ContactInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactInfoController {
 
+    @Autowired
+    private ContactInfoDAO contactInfoDAO;
+
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public void userInfoRequest(@RequestBody ContactInfo userInfo) {
+        contactInfoDAO.addContactInfo(userInfo);
+    }
 }
